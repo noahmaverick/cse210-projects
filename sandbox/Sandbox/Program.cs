4a77514eabedd4f1e2c03331b5391822af49f9a2
noahmaverick <scistarobserver41@gmail.com>
2026-04-23 10:23:46 -0600

    Noah Cobb finished prep 1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World!!!");
+        Console.WriteLine("This is in C#.");
+
+        Console.WriteLine("What is your favorite color? ");
+        string color = Console.ReadLine();
+
+        Console.WriteLine($"Your color is {color}");
+        
+        
+        
     }
 }

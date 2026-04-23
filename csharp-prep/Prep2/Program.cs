92d248a52fd8003be4fa6ab45118544c19be7c9f
noahmaverick <scistarobserver41@gmail.com>
2026-04-23 11:54:34 -0600

    Noah Cobb did the project

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+    
+        Console.Write("What is your score?: ");
+        string score = Console.ReadLine();
+
+        int scorefromuser = int.Parse(score);
+        if (scorefromuser >= 90)
+        {
+            Console.WriteLine("you have received an A on this project");
+            
+        }
+        else if (scorefromuser >= 80)
+        {
+            Console.WriteLine("You received a B on the project"); 
+        }
+        else if (scorefromuser >= 70)
+        {
+            Console.WriteLine ("you have received a C on this project");
+        }
+        else if (scorefromuser >= 60)
+        {
+            Console.WriteLine ("You have received a D on the project");
+        }
+        else
+        {
+            Console.WriteLine("You get an F on the project, chud. ");
+        }
+        Console.WriteLine("that is your grade");
+        if (scorefromuser >= 70)
+        {
+            Console.WriteLine("You dun passed the class boi ");
+        }
+        else 
+        {
+            Console.WriteLine ("You have failed the class, boy ");
+        }
     }
 }
